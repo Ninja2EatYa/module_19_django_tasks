b13489a64a3c9390a1be6564df0bdd7e137d1471
Holden <777jamesholden@gmail.com>
2024-11-07 00:08:00 +0300

    task_19_1

--- Project_19/task1/models.py
+++ Project_19/task1/models.py
@@ -4,20 +4,20 @@ from django.db import models
 
 
 class Buyer(models.Model):
-    name = models.CharField(max_length=50, name='Имя покупателя')  # username аккаунта
-    balance = models.DecimalField(max_digits=10, decimal_places=2, name='Баланс')
-    age = models.IntegerField(name='Возраст')
+    name = models.CharField(max_length=50)  # username аккаунта
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    age = models.IntegerField()
 
     def __str__(self):
         return self.name
 
 
 class Game(models.Model):
-    title = models.CharField(max_length=50, name='Название игры')
-    cost = models.DecimalField(max_digits=10, decimal_places=2, name='Цена')
-    size = models.DecimalField(max_digits=10, decimal_places=2, name='Размер файлов игры')
-    description = models.TextField(name='Описание')
-    age_limited = models.BooleanField(default=False, name='Ограничение возраста 18+')
+    title = models.CharField(max_length=50)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    size = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    age_limited = models.BooleanField(default=False)
     buyer = models.ManyToManyField(Buyer, related_name='game')
 
     def __str__(self):
